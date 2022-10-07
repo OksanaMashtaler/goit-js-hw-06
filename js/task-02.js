18e8b711aead8859f5a3c23d3244e02d25f7ac56
OksanaMashtaler <mashoksana@gmail.com>
2022-10-07 16:49:27 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const liArray = [];
+
+ingredients.forEach((element) => {
+  const newLi = document.createElement("li");
+  newLi.classList.add("item");
+  newLi.textContent = element;
+  liArray.push(newLi);
+});
+
+const $ingredients = document.querySelector("#ingredients");
+$ingredients.append(...liArray);
