20c50a49c142cb9d3e8bd47fb813b17fc58b2053
OksanaMashtaler <mashoksana@gmail.com>
2022-10-08 10:32:37 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,10 @@ const inputRef = document.querySelector("#validation-input");
 const value = inputRef.getAttribute("data-length");
 
 inputRef.addEventListener("blur", () => {
-  if (value === value.length) {
+  if (Number(value) === inputRef.value.length) {
     inputRef.classList.add("valid");
     inputRef.classList.remove("invalid");
-  }
-  {
+  } else {
     inputRef.classList.add("invalid");
     inputRef.classList.remove("valid");
   }
