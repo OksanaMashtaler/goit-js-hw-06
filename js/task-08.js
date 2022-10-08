aaa4379eacab62f4db7079a4bcec3f9dc1f29eb6
OksanaMashtaler <mashoksana@gmail.com>
2022-10-08 13:42:28 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+// const formRef = document.querySelector(".login-form");
+// formRef.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   console.log(".login-form click");
+// });
 
+const formRef = document.querySelector(".login-form");
+
+formRef.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("All fields must be filled");
+  } else {
+    const formAproved = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formAproved);
+  }
+
+  event.currentTarget.reset();
+}
